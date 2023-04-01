1b4e994cb98d90fe221728251709940cdd324bd6
Doppel-13 <santiagoruizyepes@gmail.com>
2023-04-01 17:12:16 -0500

    Creation of properties in entity Tickets

--- ConcertDB/ConcertDB/DAL/Entities/Tickets.cs
+++ ConcertDB/ConcertDB/DAL/Entities/Tickets.cs
@@ -1,6 +1,18 @@
-﻿namespace ConcertDB.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConcertDB.DAL.Entities
 {
     public class Tickets
     {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        public DateTime? UseDate { get; set; }
+
+        public Boolean IsUsed { get; set; } 
+
+        public String? EntranceGate { get; set; }    
+
     }
 }
